4ab02d9c19df18110876d4ef03dcc83468f3fcdf
pabsannav1 <pabsannav1@gmail.com>
2022-01-27 16:18:06 +0100

    Update game.js

--- game.js
+++ game.js
@@ -4,16 +4,16 @@ let loser = 'Has perdido';
 let draw = 'Has empatado';
 
 function computerPlay(options=option) {
-    let computerSelection = options[Math.floor(Math.random()*options.length)];
-    return computerSelection;
+    const computerSelection = options[Math.floor(Math.random()*options.length)];
+    return console.log(computerSelection);
 }
 
 function playerPlay() {
-    let playerSelection1 = prompt('Elige tu jugada: piedra, papel o tijeras');
-    playerSelection2 = playerSelection1.toUpperCase();
+    const playerSelection1 = prompt('Elige tu jugada: piedra, papel o tijeras');
+    const playerSelection2 = playerSelection1.toUpperCase();
 
     if (option.indexOf(playerSelection2) != -1) {
-        playerSelection = playerSelection2;
+        const playerSelection = playerSelection2;
         return console.log(playerSelection);
     } else {
         const inputerror = 'No ha escrito bien su elección';
@@ -21,11 +21,33 @@ function playerPlay() {
     }
 }
 
-computerPlay();
-playerPlay();
+computerSelection = computerPlay();
+playerSelection = playerPlay();
 
-function Play() {
-    if (playerSelection === 'PIEDRA' && computerSelection === 'TIJERAS' {
+console.log(computerSelection === playerSelection);
 
+function Play(playerSelection, computerSelection) {
+    if (playerSelection === 'PIEDRA' && computerSelection === 'PIEDRA') {
+        return draw;
+    } else if (playerSelection === 'PIEDRA' && computerSelection === 'PAPEL') {
+        return loser;
+    } else if (playerSelection === 'PIEDRA' && computerSelection === 'TIJERAS') {
+        return winner;
+    } else if (playerSelection === 'PAPEL' && computerSelection === 'PIEDRA') {
+        return winner;
+    } else if (playerSelection === 'PAPEL' && computerSelection === 'PAPEL') {
+        return draw;
+    } else if (playerSelection === 'PAPEL' && computerSelection === 'TIJERAS') {
+        return loser;
+    } else if (playerSelection === 'TIJERAS' && computerSelection === 'PIEDRA') {
+        return loser;
+    } else if (playerSelection === 'TIJERAS' && computerSelection === 'PAPEL') {
+        return winner;
+    } else if (playerSelection === 'TIJERAS' && computerSelection === 'PAPEL') {
+        return draw;
+    } else {
+        return 'error fatal'
     }
 }
+
+console.log(Play());
